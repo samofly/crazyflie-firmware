f3fd24ec747d54aace8ca7dc266a0c9407acd89d
Ivan Krasin <imkrasin@gmail.com>
2013-10-14 00:26:19 +0000

    Log raw values from mpu6050GetMotion6, instead of values from imu filter

--- modules/src/stabilizer.c
+++ modules/src/stabilizer.c
@@ -31,6 +31,7 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#include "mpu6050.h"
 #include "system.h"
 #include "stabilizer.h"
 #include "commander.h"
@@ -43,91 +44,15 @@
 #include "debug.h"
 #include "crtp.h"
 
-/**
- * Defines in what divided update rate should the attitude
- * control loop run relative the rate control loop.
- */
-#define ATTITUDE_UPDATE_RATE_DIVIDER 2
-#define FUSION_UPDATE_DT                                                       \
-  (float)(1.0 / (IMU_UPDATE_FREQ / ATTITUDE_UPDATE_RATE_DIVIDER))
-
-#define LOGGING_ENABLED
-#ifdef LOGGING_ENABLED
-#define PRIVATE
-#else
-#define PRIVATE static
-#endif
-
-PRIVATE Axis3f gyro; // Gyro axis data in deg/s
-PRIVATE Axis3f acc;  // Accelerometer axis data in mG
-
-PRIVATE float eulerRollActual;
-PRIVATE float eulerPitchActual;
-PRIVATE float eulerYawActual;
-PRIVATE float eulerRollDesired;
-PRIVATE float eulerPitchDesired;
-PRIVATE float eulerYawDesired;
-PRIVATE float rollRateDesired;
-PRIVATE float pitchRateDesired;
-PRIVATE float yawRateDesired;
-PRIVATE float fusionDt;
-
-RPYType rollType;
-RPYType pitchType;
-RPYType yawType;
-
-uint16_t actuatorThrust;
-int16_t actuatorRoll;
-int16_t actuatorPitch;
-int16_t actuatorYaw;
-
-uint32_t motorPowerM4;
-uint32_t motorPowerM2;
-uint32_t motorPowerM1;
-uint32_t motorPowerM3;
-
-LOG_GROUP_START(stabilizer)
-LOG_ADD(LOG_FLOAT, roll, &eulerRollActual)
-LOG_ADD(LOG_FLOAT, pitch, &eulerPitchActual)
-LOG_ADD(LOG_FLOAT, yaw, &eulerYawActual)
-LOG_ADD(LOG_UINT16, thrust, &actuatorThrust)
-LOG_GROUP_STOP(stabilizer)
-
-LOG_GROUP_START(motor)
-LOG_ADD(LOG_INT32, m4, &motorPowerM4)
-LOG_ADD(LOG_INT32, m1, &motorPowerM1)
-LOG_ADD(LOG_INT32, m2, &motorPowerM2)
-LOG_ADD(LOG_INT32, m3, &motorPowerM3)
-LOG_GROUP_STOP(motor)
-
-LOG_GROUP_START(acc)
-LOG_ADD(LOG_FLOAT, x, &acc.x)
-LOG_ADD(LOG_FLOAT, y, &acc.y)
-LOG_ADD(LOG_FLOAT, z, &acc.z)
-LOG_GROUP_STOP(acc)
-
-LOG_GROUP_START(gyro)
-LOG_ADD(LOG_FLOAT, x, &gyro.x)
-LOG_ADD(LOG_FLOAT, y, &gyro.y)
-LOG_ADD(LOG_FLOAT, z, &gyro.z)
-LOG_GROUP_STOP(gyro)
+static void stabilizerTask(void *param);
 
 static bool isInit;
-
-static void stabilizerTask(void *param);
 
 void stabilizerInit(void) {
   if (isInit)
     return;
 
-  motorsInit();
   imu6Init();
-  sensfusion6Init();
-  controllerInit();
-
-  rollRateDesired = 0;
-  pitchRateDesired = 0;
-  yawRateDesired = 0;
 
   xTaskCreate(stabilizerTask, (const signed char * const) "STABILIZER",
               2 * configMINIMAL_STACK_SIZE, NULL, /*Piority*/ 2, NULL);
@@ -136,10 +61,11 @@ void stabilizerInit(void) {
 }
 
 bool stabilizerTest(void) {
-  return motorsTest() & imu6Test() & sensfusion6Test() & controllerTest();
+  return imu6Test();
 }
 
 static CRTPPacket p;
+static int16_t mpuOut[6];
 
 static void stabilizerTask(void *param) {
   uint32_t lastWakeTime;
@@ -154,14 +80,13 @@ static void stabilizerTask(void *param) {
   while (1) {
     vTaskDelayUntil(&lastWakeTime, F2T(IMU_UPDATE_FREQ));
 
-    imu6Read(&gyro, &acc);
+    mpu6050GetMotion6(&mpuOut[0], &mpuOut[1], &mpuOut[2], &mpuOut[3], &mpuOut[4], &mpuOut[5]);
     uint64_t imuReadTime = usecTimestamp();
 
-    p.size = 30;
+    p.size = 21;
     p.data[0] = 133;
-    memcpy(&p.data[1], &gyro, sizeof(gyro));
-    memcpy(&p.data[13], &acc, sizeof(acc)); 
-    memcpy(&p.data[25], &imuReadTime, 6);
+    memcpy(&p.data[1], mpuOut, 12);
+    memcpy(&p.data[13], &imuReadTime, 8);
     crtpSendPacket(&p);
   }
 }
